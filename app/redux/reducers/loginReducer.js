0c5f08a021057606f2d68eb665de8e02346349b0
rupeshm@mitrmedia.com <parabsachin001@gmail.com>
2023-04-08 23:23:00 +0530

    Updated

--- app/redux/reducers/loginReducer.js
+++ app/redux/reducers/loginReducer.js
@@ -1,13 +1,12 @@
 const initialState = {
     userStatus: {
-        status: false
+        status: true
     }
 }
 
 const LoginReducer = function (state = initialState, action) {
     switch (action.type) {
         case 'SET_LOGGIN_STATUS':
-            console.log(action, 'action');
             return {
                 ...state,
                 userStatus: {
